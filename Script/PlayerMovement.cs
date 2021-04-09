c0698655d8e657d52b11de351e449e2d3d0c320f
Sandwich-God <reubensandwichclemens@gmail.com>
2021-04-08 23:22:36 -0400

    change
    Changed cs to allow the player to change the animation depending on the direction there going.

--- Script/PlayerMovement.cs
+++ Script/PlayerMovement.cs
@@ -7,10 +7,9 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f; // Variable for players speed.
 
     public Rigidbody2D rb; // Variable for getting values for Movement.
-    public Camera cam; // Variable for getting values for mouse position.
+    public Animator animator;
 
     Vector2 movement; // Contains the x and y value for movement.
-    Vector2 mousePos; // Contains the x and y value for mousePos.
 
     // Update is called once per frame
     void Update()
@@ -18,17 +17,15 @@ public class PlayerMovement : MonoBehaviour
         // Input
         movement.x = Input.GetAxisRaw("Horizontal"); // Inputs the value for x in movement.
         movement.y = Input.GetAxisRaw("Vertical"); //Inputs the value for y in movement.
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition); //Gets the position of the mouse.
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
     void FixedUpdate()
     {
         // Movement
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime); // Moves the character in the direction needed.
-/* Commenting out right now because I need to change it to the weapon instead.
-        Vector2 lookDir = mousePos - rb.position; // Subtracts the two positions to get a new position.
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 270f; // Changes the characters facing direction to the mouse position.
-        rb.rotation = angle; // Changes the rotation for character.
-*/
     }
 }
 // testing
