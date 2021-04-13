3299a862514456a47c1806489f20f0be5ea70f20
Sandwich-God <reubensandwichclemens@gmail.com>
2021-04-12 20:41:26 -0400

    Created
    Created a new C# script with the purpose of using it as a system to shoot arrows.

---
+++ Script/Shooting.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject arrowPrefab;
+
+    public float arrowForce = 15f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        GameObject arrow = Instantiate(arrowPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = arrow.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.right * arrowForce, ForceMode2D.Impulse);
+    }
+}
