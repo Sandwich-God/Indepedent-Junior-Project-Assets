d19f39bf82b1d00fe3595a5ea7526cef13ed7be6
Sandwich-God <reubensandwichclemens@gmail.com>
2021-04-14 23:16:30 -0400

    Commenting
    Commented on both Arrow and EnemyTemp script.

--- Script/EnemyTemp.cs
+++ Script/EnemyTemp.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class EnemyTemp : MonoBehaviour
 {
-    public GameObject EnemyT;
-    public float health = 2f;
+
+    public GameObject EnemyT; // Makes the Enemy Sprite into an GameObject to be changed in script
+    public float health = 2f; // Sets a variable where Enemies health is 2.
 
     // Update is called once per frame
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision) // Detects the collision with other objects.
     {
-        
-        health -= 1;
-        if (health == 0)
+
+        health -= 1; // Decrements the Enemies health.
+        if (health == 0) // States that if the health is 0, it will destroy the gameObject/Enemy.
         {
-            Destroy(gameObject);
+ 
+            Destroy(gameObject); // Destroys gameObject/Enemy
         }
     }
 }
