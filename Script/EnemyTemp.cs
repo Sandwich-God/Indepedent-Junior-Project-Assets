b38d4a67c23e8d85403f860cc45fde63d9c1b68c
Sandwich-God <reubensandwichclemens@gmail.com>
2021-04-15 20:05:06 -0400

    Added
    Added onto the EnemyTemp script to make the Enemy follow the player.

--- Script/EnemyTemp.cs
+++ Script/EnemyTemp.cs
@@ -6,9 +6,26 @@ public class EnemyTemp : MonoBehaviour
 {
 
     public GameObject EnemyT; // Makes the Enemy Sprite into an GameObject to be changed in script
-    public float health = 2f; // Sets a variable where Enemies health is 2.
+    public float speed;
+    public float health;
+    private Transform target;
 
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>(); // Sets the player as the target to find.
+    }
     // Update is called once per frame
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, target.position) > 1) // Limits the enemy from going into the same position as player.
+            {
+            // Moves the enemy into the position of the player.
+                transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            }
+        
+    }
+
     void OnCollisionEnter2D(Collision2D collision) // Detects the collision with other objects.
     {
 
