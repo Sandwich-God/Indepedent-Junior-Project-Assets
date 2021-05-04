f417073e81925a749e43acf75e304c3db9b1cd69
Sandwich-God <reubensandwichclemens@gmail.com>
2021-05-03 23:02:59 -0400

    Commented
    Commented on the HealthBar script.

--- Script/Player/HealthBar.cs
+++ Script/Player/HealthBar.cs
@@ -5,16 +5,16 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    public Slider slider;
+    public Slider slider; // Calls to the slider on the UI.
     
-    public void SetMaxHealth(int health)
+    public void SetMaxHealth(int health) // Sets the max healthon the UI slider.
     {
-        slider.maxValue = health;
-        slider.value = health;
+        slider.maxValue = health; // Sets the maxvalue for slider.
+        slider.value = health; // Sets the interchangable slider.
     }
 
-    public void SetHealth(int health)
+    public void SetHealth(int health) // Sets the health value on the sldier.
     {
-        slider.value = health;
+        slider.value = health; // Changes the slider for the health bar to current health. Used in Player script.
     }
 }
