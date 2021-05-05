7645736344177f742123bf6d967dd21450544932
Sandwich-God <reubensandwichclemens@gmail.com>
2021-05-04 23:54:23 -0400

    commented
    Commented on WeaponSwitcher for reminding me of what variable and function did.

--- Script/Player/WeaponSwitcher.cs
+++ Script/Player/WeaponSwitcher.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class WeaponSwitcher : MonoBehaviour
 {
-    public int selectedWeapon = 0;
+    public int selectedWeapon = 0; // Variable is created to allow a change of weapon based on number.h
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +14,7 @@ public class WeaponSwitcher : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        int previousSelectedWeapon = selectedWeapon;
+        int previousSelectedWeapon = selectedWeapon; // Creates a new variable based off the selectedWeapon previous value
         if (Input.GetKeyDown(KeyCode.Alpha1)) // States that if the Input is left click it will fire an arrow.
         {
             selectedWeapon = 0;
@@ -23,12 +23,12 @@ public class WeaponSwitcher : MonoBehaviour
         {
             selectedWeapon = 1;
         }
-        if (previousSelectedWeapon != selectedWeapon)
+        if (previousSelectedWeapon != selectedWeapon) // Calls to SelectWeapon() function.
         {
             SelectWeapon();
         }
     }
-    void SelectWeapon()
+    void SelectWeapon() // Selects the weapon based on the selectedWeapon value and either turns on or off the gameobject.
     {
         int i = 0;
         foreach(Transform weapon in transform)
